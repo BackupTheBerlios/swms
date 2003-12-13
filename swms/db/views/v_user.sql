d96758a52f42c7be2f31e58afb1197cbe56882e4
quetschi <quetschi>
2003-12-13 19:29:57 +0000

    created view to get list of  users

---
+++ swms/db/views/v_user.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE FORCE VIEW SWMS.V_USER
+(ID, FIRST_NAME, LAST_NAME, DEPARTMENT, EMPLOYEE_ID, 
+ USER_NAME)
+AS 
+select id, first_name, last_name, department, employee_id, user_name from dat_user;
